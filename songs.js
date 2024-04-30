5d8e2be1e03a102205f167f4d0e9ee929d5f7516
kkaun <133983953+ddeviljn@users.noreply.github.com>
2024-04-30 01:16:49 -0300

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,14 @@
+export default [
+    {
+      src: "./src/olho.mpeg",
+      name: "ANAVITÓRIA_Lagum - Universo de coisas que eu desconheço",
+      
+    },
+    {
+      src: "./src/Gone.mpeg",
+      name: "Phillip Phillips - Gone, Gone, Gone",
+    },
+    {
+      src: "./src/Mirrors.mpeg",
+      name: "Justin Timberlake - Mirrors",
+    }];
